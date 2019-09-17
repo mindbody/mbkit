08dc7f7cce96036e539f91680e3a1ead1add92c0
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-09-17 07:47:05 -0700

    feat(docs): added ability to create pages in contentful, generating sitemap, removing prettier from docs

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -1,44 +1,44 @@
 module.exports = {
-  siteMetadata: {
-    title: `MINDBODY | Design System`,
-    description: `Discover and learn our best practices for design, copywriting, feature development and component library`,
-    author: `@czaas`,
-  },
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-layout`,
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: "2rb53inahems",
-        accessToken: "N5bVC4fM-z13KhhJB25teCb420y2krFysdjlsy_wI9E",
-      },
+    siteMetadata: {
+        title: `MINDBODY | Design System`,
+        description: `Discover and learn our best practices for design, copywriting, feature development and component library`,
+        author: `@czaas`,
     },
-    `@contentful/gatsby-transformer-contentful-richtext`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    plugins: [
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-layout`,
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: '2rb53inahems',
+                accessToken: 'N5bVC4fM-z13KhhJB25teCb420y2krFysdjlsy_wI9E',
+            },
+        },
+        `@contentful/gatsby-transformer-contentful-richtext`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+};
--- packages/docs/src/components/Nav/Nav.js
+++ packages/docs/src/components/Nav/Nav.js
@@ -1,135 +1,153 @@
-import React from "react"
-import styles from "./Nav.module.scss"
-import { Link } from "gatsby"
-const classnames = require("classnames")
+import React from 'react';
+import styles from './Nav.module.scss';
+import { Link } from 'gatsby';
+const classnames = require('classnames');
 
-// a menu should be `ul > li`, nested if needed
-export const menu = [
-  {
-    title: "Dashboard",
-    to: null,
-    newTab: false,
-    menu: [
-      {
-        title: "Business Overview",
-        to: "/business-overview",
-        menu: [
-          {
-            title: "Another",
-            to: "/business-overview",
-            menu: [],
-          },
-          {
-            title: "Test",
-            to: "/business-test",
-            menu: [],
-          },
-        ],
-      },
-      {
-        title: "Classes",
-        to: "/classes",
-        menu: [
-          {
-            title: "Yoga",
-            to: "yoga",
-            menu: [],
-          },
-          {
-            title: "Spin",
-            to: "spin",
-            menu: [],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: "Account",
-    to: "/account",
-    newTab: false,
-    menu: [
-      {
-        title: "Sign In",
-        to: "sign-in",
-        newTab: false,
-        menu: [],
-      },
-      {
-        title: "Sign Up",
-        to: "sign-up",
-        newTab: false,
-        menu: [],
-      },
-    ],
-  },
-]
+const noop = () => {};
 
-const noop = () => {}
+export function SiteMapNav(props) {
+    const { allPages, ...rest } = props;
+    const [menuItems, setMenuItems] = React.useState([]);
+    React.useEffect(() => {
+        try {
+            let items = [];
+            const itemsSorted = allPages.allSitePage.edges.sort((a, b) => a.node.path.length - b.node.path.length);
+            itemsSorted.forEach(page => {
+                const { path } = page.node;
+                if (path.includes('404')) {
+                    return;
+                }
+
+                const depth = path.split('/');
+                const sectionTitle = depth[1] === '' ? 'Home' : depth[1].replace(/-/gi, ' ');
+
+                switch (depth.length) {
+                    // Top level nav
+                    case 2:
+                        items.push({ to: path, title: sectionTitle });
+                        break;
+                    // secondary level nav
+                    case 3:
+                        // find parent;
+                        // if doesn't exist, create it
+                        // else add to child
+                        const childTitle = depth[2];
+                        let found = false;
+
+                        items = items.map(item => {
+                            const { title = '', menu = [], to = '' } = item;
+                            if (title === sectionTitle) {
+                                found = true;
+
+                                return {
+                                    ...item,
+                                    menu: [
+                                        ...menu,
+                                        {
+                                            title: childTitle.replace(/-/g, ' '),
+                                            to: path,
+                                        },
+                                    ],
+                                };
+                            }
+                            return item;
+                        });
+
+                        if (!found) {
+                            depth.pop();
+                            const parentPath = depth.join('/');
+                            items.push({
+                                title: sectionTitle,
+                                to: parentPath,
+                                menu: [
+                                    {
+                                        title: childTitle,
+                                        to: path,
+                                    },
+                                ],
+                            });
+                        }
+                        break;
+                    default:
+                        console.log(path);
+                }
+            });
+
+            setMenuItems(items);
+        } catch (e) {}
+    }, [allPages]);
+
+    return <Nav menu={menuItems} {...rest} />;
+}
+
 export default function Nav(props) {
-  const { menu = [], className, onClick = noop, ...rest } = props
-  const [visibleChildren, setVisibleChildren] = React.useState(
-    menu.map(item => ({ isVisible: true, title: item.title }))
-  )
+    const { menu = [], className, onClick = noop, ...rest } = props;
 
-  function handleVisibleChange(item) {
-    const updatedVisiblity = visibleChildren.map(c => {
-      if (c.title === item.title) {
-        return {
-          ...c,
-          isVisible: !c.isVisible,
+    const [visibleChildren, setVisibleChildren] = React.useState(
+        menu.map(item => ({ isVisible: true, title: item.title })),
+    );
+
+    React.useEffect(() => {
+        if (menu.length > 0 && visibleChildren.length === 0) {
+            menu.map(item => ({ isVisible: true, title: item.title }));
         }
-      }
-      return c
-    })
-    setVisibleChildren(updatedVisiblity)
-  }
-  // TODO
-  // auto detect mobile and show floating hamburger
-  // linkAs which allows change of anchor link
-  if (menu.length === 0) {
-    return null
-  }
+    }, [menu]);
 
-  const navClassName = classnames({
-    [styles.menu]: true,
-    [className]: className,
-  })
-  return (
-    <nav className={navClassName} {...rest}>
-      <ul>
-        {menu.map(item => {
-          const hasChildren = item.menu && item.menu.length > 0
-          const { isVisible } = visibleChildren.find(
-            c => c.title === item.title
-          )
-          const buttonClassnames = classnames({
-            [styles.menu__item__toggleChildren]: true,
-            [styles.menu__item__toggleChildrenOpened]: isVisible,
-          })
-          return (
-            <li key={item.title}>
-              <div className={styles.menu__item}>
-                {(item.to && (
-                  <Link onClick={onClick} to={item.to}>
-                    {item.title}
-                  </Link>
-                )) ||
-                  item.title}{" "}
-                {hasChildren && (
+    function handleVisibleChange(item) {
+        const updatedVisibility = visibleChildren.map(c => {
+            if (c.title === item.title) {
+                return {
+                    ...c,
+                    isVisible: !c.isVisible,
+                };
+            }
+            return c;
+        });
+        setVisibleChildren(updatedVisibility);
+    }
+
+    if (menu.length === 0) {
+        return null;
+    }
+
+    const navClassName = classnames({
+        [styles.menu]: true,
+        [className]: className,
+    });
+    return (
+        <nav className={navClassName} {...rest}>
+            <ul>
+                {menu
+                    .sort((a, b) => a.to[0] > b.to[0])
+                    .map(item => {
+                        const hasChildren = item.menu && item.menu.length > 0;
+                        const { isVisible } = visibleChildren.find(c => c.title === item.title) || {};
+                        const buttonClassnames = classnames({
+                            [styles.menu__item__toggleChildren]: true,
+                            [styles.menu__item__toggleChildrenOpened]: isVisible,
+                        });
+
+                        return (
+                            <li key={item.title}>
+                                <div className={styles.menu__item}>
+                                    {(item.to && (
+                                        <Link onClick={onClick} to={item.to}>
+                                            {item.title}
+                                        </Link>
+                                    )) ||
+                                        item.title}{' '}
+                                    {/* {hasChildren && (
                   <button
                     onClick={() => handleVisibleChange(item)}
                     className={buttonClassnames}
                   />
-                )}
-              </div>
-              {isVisible && hasChildren && (
-                <Nav menu={item.menu} onClick={onClick} />
-              )}
-            </li>
-          )
-        })}
-      </ul>
-    </nav>
-  )
+                )} */}
+                                </div>
+                                {hasChildren && <Nav menu={item.menu} onClick={onClick} />}
+                            </li>
+                        );
+                    })}
+            </ul>
+        </nav>
+    );
 }
