662cf7f2a01cb7ec37367e8efcddc40682881207
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-10-01 15:04:11 -0700

    feat(typography): added initial heading component

---
+++ packages/Typography/src/Heading.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './Typography.scss';
+
+export interface HeadingProps extends React.HtmlHTMLAttributes<HTMLHeadingElement> {
+    /** This will be the actual rendered element. Keep your headings and content semantic */
+    as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+    /** Use this when you need to render some heading element but it needs to look a different size */
+    size?: 1 | 2 | 3 | 4 | 5 | 6;
+}
+const Heading: React.FC<HeadingProps> = (props: HeadingProps & Partial<HTMLHeadingElement>) => {
+    const { as, className = '', size, ...rest } = props;
+    const Component = as;
+    const computedSize = size !== undefined ? `h${size}` : as;
+
+    const classNames = classnames({
+        [styles.h1]: computedSize === 'h1',
+        [styles.h2]: computedSize === 'h2',
+        [styles.h3]: computedSize === 'h3',
+        [styles.h4]: computedSize === 'h4',
+        [styles.h5]: computedSize === 'h5',
+        [styles.h6]: computedSize === 'h6',
+        [className]: className,
+    });
+    return <Component {...rest} className={classNames} />;
+};
+
+export default Heading;
