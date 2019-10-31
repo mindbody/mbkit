12cf7781e4c51886fd080be9505433fdac8940f6
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-10-31 12:33:13 -0700

    feat(card): added new card package

---
+++ packages/Card/src/Card.tsx
@@ -0,0 +1,20 @@
+import React, { FC, HTMLAttributes, forwardRef, RefObject, ReactElement } from 'react';
+import classnames from 'classnames';
+import styles from './Card.scss';
+
+export type CardProps = HTMLAttributes<HTMLElement> & {
+    as?: ReactElement;
+    elevation?: 0 | 1 | 2 | 3 | 4;
+};
+export const Card: FC<CardProps> = forwardRef((props: CardProps, ref: RefObject<HTMLElement>) => {
+    const { as = 'div' as any, elevation = 1, className = '', ...rest } = props;
+    const Component = as;
+
+    const classNames = classnames({
+        [styles.card]: true,
+        [styles[`elevation${elevation}`]]: true,
+        [className]: className,
+    });
+    return <Component {...rest} className={classNames} ref={ref} />;
+});
+Card.displayName = 'Card';
