ec30fb992f0a49ff052e10e35188aeb0ea3ee26e
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-11-04 09:49:36 -0800

    test(banner): added variant test and added custom icon test

--- packages/Banner/src/__tests__/Banner.test.tsx
+++ packages/Banner/src/__tests__/Banner.test.tsx
@@ -6,12 +6,34 @@ describe('Banner', () => {
     const title = "Congratulations, you're business is setup";
     const message = "You're ready to get started making the world a healthier place";
 
-    it('should render', () => {
-        const { getByTestId } = render(<Banner variant="success" data-testid="test" />);
-        expect(getByTestId('test')).toBeTruthy();
+    it('should render each variant', () => {
+        const { getByTestId } = render(
+            <>
+                <Banner variant="success" data-testid="success" />
+                <Banner variant="warning" data-testid="warning" />
+                <Banner variant="info" data-testid="info" />
+                <Banner variant="error" data-testid="error" />
+            </>,
+        );
+        const success = getByTestId('success');
+        const warning = getByTestId('warning');
+        const info = getByTestId('info');
+        const error = getByTestId('error');
+
+        expect(success).toBeTruthy();
+        expect(success.classList.contains('success')).toBe(true);
+
+        expect(warning).toBeTruthy();
+        expect(warning.classList.contains('warning')).toBe(true);
+
+        expect(info).toBeTruthy();
+        expect(info.classList.contains('info')).toBe(true);
+
+        expect(error).toBeTruthy();
+        expect(error.classList.contains('error')).toBe(true);
     });
     it('should render with text provided', () => {
-        const { getByText } = render(<Banner variant="success" title={title} message={message} />);
+        const { getByText } = render(<Banner variant="warning" title={title} message={message} />);
         expect(getByText(title)).toBeTruthy();
         expect(getByText(message)).toBeTruthy();
     });
@@ -19,7 +41,7 @@ describe('Banner', () => {
         const { getByTestId } = render(
             <>
                 <Banner variant="success" data-testid="section" as={'section'} />
-                <Banner variant="success" data-testid="main" as={'main'} />
+                <Banner variant="error" data-testid="main" as={'main'} />
             </>,
         );
         const section = getByTestId('section');
@@ -60,5 +82,10 @@ describe('Banner', () => {
             </Banner>,
         );
         expect(getByText('Super awesome components assemble!')).toBeTruthy();
+    });
+    it('should allow you to pass a custom icon', () => {
+        const { getByTestId } = render(<Banner variant="info" icon={<div data-testid="imposterIcon" />} />);
+
+        expect(getByTestId('imposterIcon')).toBeTruthy();
     });
 });
