3f44a3e86b04f545f861c08756ae8fd95881a5d7
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-11-22 07:02:34 -0800

    refactor(card): removed elevation prop

--- packages/Card/src/Card.tsx
+++ packages/Card/src/Card.tsx
@@ -4,15 +4,13 @@ import styles from './Card.scss';
 
 export type CardProps = HTMLAttributes<HTMLElement> & {
     as?: ReactNode;
-    elevation?: 0 | 1 | 2 | 3 | 4;
 };
 export const Card: FC<CardProps> = forwardRef((props: CardProps, ref: RefObject<HTMLElement>) => {
-    const { as = 'div' as any, elevation = 1, className = '', ...rest } = props;
+    const { as = 'div' as any, className = '', ...rest } = props;
     const Component = as;
 
     const classNames = classnames({
         [styles.card]: true,
-        [styles[`elevation${elevation}`]]: true,
         [className]: className,
     });
     return <Component {...rest} className={classNames} ref={ref} />;
