c5031428967d2165b9691540f04c11d61098277c
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-12-02 10:49:18 -0800

    feat(file-uploader): added a11y label, added loading state

--- packages/FileUploader/src/FileUploader.tsx
+++ packages/FileUploader/src/FileUploader.tsx
@@ -13,12 +13,21 @@ import classnames from 'classnames';
 import styles from './FileUploader.scss';
 
 export type FileUploaderProps = AllHTMLAttributes<HTMLInputElement> & {
+    /** Shows button in different variant */
     variant?: 'primary' | 'secondary' | 'primaryOutlined' | 'secondaryOutlined';
+    /** Text that shows up in the input field when there is no value. (Do not rely on this for important information) */
     placeholder?: ReactNode;
+    /** Text that shows up in the button */
     buttonLabel?: ReactNode;
+    /** Shows loading indicator */
     loading?: boolean;
+    /** Shows red outline around file uploader */
     invalid?: boolean;
+    /** A11y attribute that reads to the user when the user has focus of the FileUploader */
+    label?: string;
+    /** Value that is displayed in the input field. If using an array of values it will automatically add the comma separators */
     value: string | string[];
+    /** Function that is called when a file is uploaded */
     onChange: (event: SyntheticEvent<HTMLInputElement>) => void;
 };
 export const FileUploader: FC<FileUploaderProps> = forwardRef(
@@ -26,11 +35,13 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
         const {
             className = '',
             value,
-            placeholder = 'Tap here or drop a file',
+            placeholder = 'To upload files tap here or drop a file',
             buttonLabel = 'Choose File',
+            label = 'File Uploader',
             invalid,
             onChange = () => {},
             variant = 'secondary',
+            loading,
             disabled,
             ...rest
         } = props;
@@ -73,6 +84,7 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
             [styles.fileUploader]: true,
             [styles.invalid]: invalid,
             [styles.isDragging]: isDragging,
+            [styles.isLoading]: loading,
             [styles.disabled]: disabled,
             [className]: className,
         });
@@ -83,23 +95,29 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
         });
         const buttonClassNames = classnames({
             [styles.button]: true,
+            [styles.isLoading]: loading,
             [styles[`variant${variant}`]]: true,
         });
 
         const fileValue = Array.isArray(value) ? formatListOfValues(value) : value;
         return (
             <div
-                tabIndex={0}
+                tabIndex={disabled ? -1 : 0}
                 onKeyDown={handleKeyPress}
                 className={classNames}
                 onDragOver={handleDragOver}
                 onDragLeave={handleDragLeave}
                 role="button"
-                aria-label="file input"
+                aria-label={label}
+                aria-busy={loading}
             >
                 <div className={displayTextClassNames}>
                     {fileValue || placeholder}
                     {isDragging && <div className={styles.largeDrop} />}
+                </div>
+                <div className={buttonClassNames}>
+                    {loading && <div className={styles.loader} />}
+                    {buttonLabel}
                 </div>
                 <input
                     type="file"
@@ -108,9 +126,8 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
                     onChange={handleFileChange}
                     tabIndex={-1}
                     className={styles.input}
-                    disabled={disabled}
+                    disabled={disabled || loading}
                 />
-                <div className={buttonClassNames}>{buttonLabel}</div>
             </div>
         );
     },
