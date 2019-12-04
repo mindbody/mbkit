b9adab0973ac9cc4233acac5ec1d454b0fb51ff5
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-12-04 13:19:04 -0800

    refactor(file-uploader): updated props to include ref. Updated value when array is passed in as initial value

--- packages/FileUploader/src/FileUploader.tsx
+++ packages/FileUploader/src/FileUploader.tsx
@@ -2,36 +2,38 @@ import React, {
     FC,
     forwardRef,
     RefObject,
-    AllHTMLAttributes,
-    useRef,
     KeyboardEvent,
     useState,
-    SyntheticEvent,
     ReactNode,
+    ChangeEvent,
+    HTMLProps,
+    useRef,
+    AllHTMLAttributes,
 } from 'react';
 import classnames from 'classnames';
 import styles from './FileUploader.scss';
 
-export type FileUploaderProps = AllHTMLAttributes<HTMLInputElement> & {
-    /** Shows button in different variant */
-    variant?: 'primary' | 'secondary' | 'primaryOutlined' | 'secondaryOutlined';
-    /** Text that shows up in the input field when there is no value. (Do not rely on this for important information) */
-    placeholder?: ReactNode;
-    /** Text that shows up in the button */
-    buttonLabel?: ReactNode;
-    /** Shows loading indicator */
-    loading?: boolean;
-    /** Shows red outline around file uploader */
-    invalid?: boolean;
-    /** A11y attribute that reads to the user when the user has focus of the FileUploader */
-    label?: string;
-    /** Value that is displayed in the input field. If using an array of values it will automatically add the comma separators */
-    value: string | string[];
-    /** Function that is called when a file is uploaded */
-    onChange: (event: SyntheticEvent<HTMLInputElement>) => void;
-};
+export type FileUploaderProps = AllHTMLAttributes<HTMLDivElement> &
+    HTMLProps<HTMLDivElement> & {
+        /** Shows button in different variant */
+        variant?: 'primary' | 'secondary' | 'primaryOutlined' | 'secondaryOutlined';
+        /** Text that shows up in the input field when there is no value. (Do not rely on this for important information) */
+        placeholder?: ReactNode;
+        /** Text that shows up in the button */
+        buttonLabel?: ReactNode;
+        /** Shows loading indicator */
+        loading?: boolean;
+        /** Shows red outline around file uploader */
+        invalid?: boolean;
+        /** A11y attribute that reads to the user when the user has focus of the FileUploader */
+        label?: string;
+        /** Value that is displayed in the input field. If using an array of values it will automatically add the comma separators */
+        value: string | string[];
+        /** Function that is called when a file is uploaded */
+        onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    };
 export const FileUploader: FC<FileUploaderProps> = forwardRef(
-    (props: FileUploaderProps, ref: RefObject<HTMLInputElement>) => {
+    (props: FileUploaderProps, ref: RefObject<HTMLDivElement>) => {
         const {
             className = '',
             value,
@@ -39,13 +41,13 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
             buttonLabel = 'Choose File',
             label = 'File Uploader',
             invalid,
-            onChange = () => {},
+            onChange,
             variant = 'secondary',
             loading,
             disabled,
             ...rest
         } = props;
-        const inputRef = useRef<HTMLInputElement>((ref && ref.current) || null);
+        const inputRef = useRef<HTMLInputElement>(null);
 
         function triggerFilUpload() {
             const input = inputRef.current;
@@ -75,11 +77,14 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
         function handleDragLeave() {
             setIsDragging(false);
         }
-        function handleFileChange(event: SyntheticEvent<HTMLInputElement>) {
+        function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+            // react needs to use this to persist the event in the passed function
+            event.persist();
             onChange(event);
             setIsDragging(false);
         }
 
+        const formattedValue = Array.isArray(value) ? formatListOfValues(value) : value;
         const classNames = classnames({
             [styles.fileUploader]: true,
             [styles.invalid]: invalid,
@@ -90,8 +95,8 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
         });
         const displayTextClassNames = classnames({
             [styles.displayText]: true,
-            [styles.placeholder]: !value,
-            [styles.value]: value,
+            [styles.placeholder]: !formattedValue,
+            [styles.value]: formattedValue,
         });
         const buttonClassNames = classnames({
             [styles.button]: true,
@@ -99,7 +104,6 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
             [styles[`variant${variant}`]]: true,
         });
 
-        const fileValue = Array.isArray(value) ? formatListOfValues(value) : value;
         return (
             <div
                 tabIndex={disabled ? -1 : 0}
@@ -110,21 +114,19 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
                 role="button"
                 aria-label={label}
                 aria-busy={loading}
+                ref={ref}
             >
-                <div className={displayTextClassNames}>
-                    {fileValue || placeholder}
-                    {isDragging && <div className={styles.largeDrop} />}
-                </div>
+                <div className={displayTextClassNames}>{formattedValue || placeholder}</div>
                 <div className={buttonClassNames}>
                     {loading && <div className={styles.loader} />}
                     {buttonLabel}
                 </div>
                 <input
+                    {...rest}
+                    tabIndex={-1}
                     type="file"
                     ref={inputRef}
-                    {...rest}
                     onChange={handleFileChange}
-                    tabIndex={-1}
                     className={styles.input}
                     disabled={disabled || loading}
                 />
@@ -135,8 +137,10 @@ export const FileUploader: FC<FileUploaderProps> = forwardRef(
 FileUploader.displayName = 'FileUploader';
 
 function formatListOfValues(list: string[]) {
-    return list.map((item, index) => {
-        const isLast = index === list.length - 1;
-        return `${item}${isLast ? '' : ', '}`;
-    });
+    return list
+        .map((item, index) => {
+            const isLast = index === list.length - 1;
+            return `${item}${isLast ? '' : ', '}`;
+        })
+        .join('');
 }
