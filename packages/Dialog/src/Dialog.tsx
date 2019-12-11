9b1e71b9d13c491c8361f2ef328124ce982c3dce
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-12-11 13:18:27 -0800

    refactor(dialog): minor adjustments to container for positioning and padding allowing user to override default padding

--- packages/Dialog/src/Dialog.tsx
+++ packages/Dialog/src/Dialog.tsx
@@ -26,15 +26,15 @@ const CloseIcon = (props: any) => (
     <svg width="32" height="32" viewBox="0 0 32 32" {...props}>
         <defs>
             <path
-                id="path-1-YCcq7N9hUYyFxWI-pg_o5"
+                id="path-1-YCcq7N9hUYyFxWI-pg_o5f"
                 d="M7.6440993 7.64644661c.19526214-.19526215.51184463-.19526215.70710678 0L15.9986527 15.294l7.6477939-7.64755339c.1952622-.19526215.5118446-.19526215.7071068 0 .1952621.19526215.1952621.51184464 0 .70710678L16.7056527 16.001l7.6479007 7.6477938c.1735663.1735663.1928515.4429907.0578554.6378589l-.0578554.0692479c-.1952622.1952621-.5118446.1952621-.7071068 0L15.9986527 16.708l-7.64744662 7.6479006c-.19526215.1952621-.51184464.1952621-.70710678 0-.19526215-.1952622-.19526215-.5118447 0-.7071068L15.2916527 16.001 7.6440993 8.35355339c-.17356635-.17356635-.1928515-.44299075-.05785545-.63785889z"
             ></path>
         </defs>
-        <g id="Icons/UI/Close-YCcq7N9hUYyFxWI-pg_o5" fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
-            <mask id="mask-2-YCcq7N9hUYyFxWI-pg_o5" fill="#fff">
-                <use xlinkHref="#path-1-YCcq7N9hUYyFxWI-pg_o5"></use>
+        <g id="Icons/UI/Close-YCcq7N9hUYyFxWI-pg_o5f" fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+            <mask id="mask-2-YCcq7N9hUYyFxWI-pg_o5f" fill="#fff">
+                <use xlinkHref="#path-1-YCcq7N9hUYyFxWI-pg_o5f"></use>
             </mask>
-            <use id="Close-YCcq7N9hUYyFxWI-pg_o5" fill="#54575F" xlinkHref="#path-1-YCcq7N9hUYyFxWI-pg_o5"></use>
+            <use id="Close-YCcq7N9hUYyFxWI-pg_o5f" fill="#54575F" xlinkHref="#path-1-YCcq7N9hUYyFxWI-pg_o5f"></use>
         </g>
     </svg>
 );
@@ -84,12 +84,12 @@ export const Dialog: FC<DialogProps> = forwardRef((props: DialogProps, ref) => {
                             {...veilProps}
                             className={`${veilClassNames} ${styles[`veil_${state}`]}`}
                         >
-                            <AlertDialogContent
-                                {...rest}
-                                className={`${classNames} ${styles[`dialog_${state}`]}`}
-                                ref={ref}
-                            >
-                                <div className={styles.content}>
+                            <div className={styles.content}>
+                                <AlertDialogContent
+                                    {...rest}
+                                    className={`${classNames} ${styles[`dialog_${state}`]}`}
+                                    ref={ref}
+                                >
                                     <AlertDialogLabel className={styles.header}>{header}</AlertDialogLabel>
                                     <AlertDialogDescription className={styles.description}>
                                         {children}
@@ -102,8 +102,8 @@ export const Dialog: FC<DialogProps> = forwardRef((props: DialogProps, ref) => {
                                     >
                                         <CloseIcon aria-hidden="true" />
                                     </button>
-                                </div>
-                            </AlertDialogContent>
+                                </AlertDialogContent>
+                            </div>
                         </AlertDialogOverlay>
                     )}
                 </>
