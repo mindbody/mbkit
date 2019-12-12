8daaf1f24dfd2d5b19691809feeb70edc182b03e
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-12-12 06:39:04 -0800

    fix(radio): added ref attributes to props

--- packages/Radio/src/Radio.tsx
+++ packages/Radio/src/Radio.tsx
@@ -1,15 +1,16 @@
-import React, { FC, RefObject, forwardRef, AllHTMLAttributes } from 'react';
+import React, { FC, RefObject, forwardRef, AllHTMLAttributes, RefAttributes } from 'react';
 import classnames from 'classnames';
 import styles from './Radio.scss';
 
-export type RadioProps = AllHTMLAttributes<HTMLInputElement> & {
-    /** Controls whether the radio is checked */
-    checked: boolean;
-    /** Fires each time the user interacts with the input */
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    /** Shows red border */
-    invalid?: boolean;
-};
+export type RadioProps = AllHTMLAttributes<HTMLInputElement> &
+    RefAttributes<HTMLInputElement> & {
+        /** Controls whether the radio is checked */
+        checked: boolean;
+        /** Fires each time the user interacts with the input */
+        onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+        /** Shows red border */
+        invalid?: boolean;
+    };
 export const Radio: FC<RadioProps> = forwardRef((props: RadioProps, ref: RefObject<HTMLInputElement>) => {
     const { className = '', invalid, ...rest } = props;
     const classNames = classnames({
