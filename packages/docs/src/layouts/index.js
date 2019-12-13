97d806fc3c0befeb793bf26b45ba46afb81c313e
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2019-12-13 13:02:46 -0800

    feat(docs): added footer links for github and dsm

--- packages/docs/src/layouts/index.js
+++ packages/docs/src/layouts/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import styles from './index.module.scss';
 import { SiteMapNav } from '../components/Nav/Nav';
@@ -31,6 +31,26 @@ const Layout = props => {
             setMobileNavOpen(false);
         }
     }
+    useEffect(() => {
+        if (mobileNavOpen) {
+            document.body.style.overflow = 'hidden';
+            window.addEventListener('resize', disableScrollOnWindowOnly);
+        } else {
+            document.body.style.overflow = '';
+            window.removeEventListener('resize', disableScrollOnWindowOnly);
+        }
+
+        return () => {
+            window.removeEventListener('resize', disableScrollOnWindowOnly);
+        };
+    }, [mobileNavOpen]);
+
+    function disableScrollOnWindowOnly() {
+        const { clientWidth } = window.document.body;
+        if (clientWidth >= 640) {
+            setMobileNavOpen(false);
+        }
+    }
     return (
         <>
             <SkipNavLink />
@@ -41,17 +61,28 @@ const Layout = props => {
                             query={query}
                             render={data => <SiteMapNav allPages={data} onClick={handleLinkClick} />}
                         />
+                        <ul className={styles.footerNav}>
+                            <li>
+                                <a
+                                    href="https://mindbodyonline.invisionapp.com/dsm/mindbody/1-mb-2-0"
+                                    target="_blank"
+                                    rel="nofollow"
+                                >
+                                    InVision DSM
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/mindbody/design-system" target="_blank" rel="nofollow">
+                                    Github
+                                </a>
+                            </li>
+                        </ul>
                     </nav>
                     <button className={styles.mobileNavButton} onClick={e => setMobileNavOpen(!mobileNavOpen)} />
                 </div>
                 <SkipNavContent>
                     <main className={styles.main}>{props.children}</main>
                 </SkipNavContent>
-            </div>
-            <div>
-                <a href="https://mindbodyonline.invisionapp.com/dsm/mindbody/1-mb-2-0" target="_blank" rel="nofollow">
-                    InVision DSM site
-                </a>
             </div>
         </>
     );
