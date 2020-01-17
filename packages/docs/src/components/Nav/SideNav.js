3ddd1e2ee32ad5c06717e5fd65cb2147b16d087f
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2020-01-16 16:21:21 -0800

    feat(docs): updated sidenav active styles

--- packages/docs/src/components/Nav/SideNav.js
+++ packages/docs/src/components/Nav/SideNav.js
@@ -28,28 +28,34 @@ export const SideNav = props => {
                     {subMenu
                         .sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1))
                         .map(item => {
-                            const sideNavClassNames = classnames({
-                                [styles.item]: true,
-                                [styles.active]: pathnameContainsCurrentPage(item.to),
-                            });
-                            const hasChildren = item.menu && item.menu.length > 0;
-                            return (
-                                <li key={item.title} className={sideNavClassNames}>
-                                    <Link to={item.to}>{item.title}</Link>
-
-                                    <ul>
-                                        {hasChildren &&
-                                            item.menu.map(subMenu => (
-                                                <li key={subMenu.to}>
-                                                    <Link to={subMenu.to}>{subMenu.title}</Link>
-                                                </li>
-                                            ))}
-                                    </ul>
-                                </li>
-                            );
+                            return <RecursiveSubNav key={item.to} {...item} />;
                         })}
                 </ul>
             </nav>
         </aside>
+    );
+};
+
+const RecursiveSubNav = props => {
+    const { title, to, menu = [] } = props;
+    const hasChildren = menu.length > 0;
+    const itemClassNames = classnames({
+        [styles.item]: true,
+        [styles.active]: pathnameContainsCurrentPage(to),
+    });
+    return (
+        <li className={itemClassNames}>
+            <Link to={to}>{title}</Link>
+
+            {hasChildren && (
+                <ul>
+                    {menu
+                        .sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1))
+                        .map(subMenu => (
+                            <RecursiveSubNav key={subMenu.to} {...subMenu} />
+                        ))}
+                </ul>
+            )}
+        </li>
     );
 };
