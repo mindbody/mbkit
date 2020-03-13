698532c20f0813442f3bc96bfee8a95ae2656fe2
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2020-03-13 13:06:15 -0700

    docs(card): updated prop documentation

--- packages/Card/src/Card.tsx
+++ packages/Card/src/Card.tsx
@@ -4,6 +4,7 @@ import styles from './Card.scss';
 
 export type CardProps = RefAttributes<HTMLElement> &
     AllHTMLAttributes<HTMLElement> & {
+        /** Renders as the element/component you pass in. E.g. div, section */
         as?: ReactNode;
     };
 export const Card: FC<CardProps> = forwardRef((props: CardProps, ref: RefObject<HTMLElement>) => {
