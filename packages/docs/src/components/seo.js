f0ae37096be7060512b4ffad933dccc40311fe2c
Brian Phillips <brian.phillips201@gmail.com>
2020-03-16 16:17:14 -0700

    fix(docs): updated social share meta info

--- packages/docs/src/components/seo.js
+++ packages/docs/src/components/seo.js
@@ -41,7 +41,7 @@ function SEO({ description, lang, meta, title }) {
                 },
                 {
                     property: `og:title`,
-                    content: title,
+                    content: `${title} | ${site.siteMetadata.title}`,
                 },
                 {
                     property: `og:description`,
@@ -65,7 +65,7 @@ function SEO({ description, lang, meta, title }) {
                 },
                 {
                     name: `twitter:title`,
-                    content: title,
+                    content: `${title} | ${site.siteMetadata.title}`,
                 },
                 {
                     name: `twitter:description`,
