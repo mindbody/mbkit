5052da3ff21356fbfc633ec29d89e9ba89509d4e
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2020-03-16 15:46:31 -0700

    chore: fixed local dev issue with lerna trying to consume toggle via symlink

--- shared/build-tools/dev-generate-local-server.js
+++ shared/build-tools/dev-generate-local-server.js
@@ -12,7 +12,9 @@ generateComponentImport();
 
 function generateComponentImport() {
     const components = `import React from 'react';
-import MarkdownWithOverrides from '../../../packages/docs/src/components/MarkdownWithOverrides/MarkdownWithOverrides';
+import Markdown from 'markdown-to-jsx';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
+import theme from 'prism-react-renderer/themes/vsDark';
 import * as components from '../${path.relative(__dirname, consumerPath)}/${pkgJson.main}';
 import Documentation from '../${path.relative(__dirname, consumerPath)}/src/${componentName}.mdx';
 import Component from "@reach/component-component";
@@ -21,31 +23,52 @@ export const GeneratedMarkdown = () => {
   const [md, setMd] = React.useState('');
 
   React.useEffect(() => {
-    fetch(Documentation)
-        .then(res=> res.text())
-        .then(content => setMd(content))
-  }, [])
-  
-  let namedExports = {}
-  Object.keys(components).forEach(key => {
-    if (key === 'default') {
-      if (typeof components[key] === 'object') {
-        namedExports = components[key];
-        return;
-      }
-      namedExports['${componentName}'] = components.default;
-      return;
-    }
-    namedExports[key] = components[key]
-    return;
-  })
+      fetch(Documentation)
+          .then(res => res.text())
+          .then(content => setMd(content));
+  }, []);
 
+  let namedExports = {};
+  Object.keys(components).forEach(key => {
+      if (key === 'default') {
+          if (typeof components[key] === 'object') {
+              namedExports = components[key];
+              return;
+          }
+          namedExports['${componentName}'] = components.default;
+          return;
+      }
+      namedExports[key] = components[key];
+      return;
+  });
   return (
-    <MarkdownWithOverrides overrides={{ Component, ...namedExports }}>
-      {md}
-    </MarkdownWithOverrides>
-  )
-}
+      <Markdown
+          options={{
+              overrides: {
+                  Component,
+                  code: props => (
+                      <LiveProvider
+                          code={props.children}
+                          scope={{
+                              Component,
+                              ...namedExports,
+                          }}
+                          theme={{ ...theme }}
+                      >
+                          <LiveEditor />
+                          <div>
+                              <LiveError />
+                              <LivePreview />
+                          </div>
+                      </LiveProvider>
+                  ),
+              },
+          }}
+      >
+          {md}
+      </Markdown>
+  );
+};
 `;
     fs.writeFile(`${__dirname}/local-server/generated.component.js`, components, err => {
         if (err) {
