d837e60e6f051561d6d27ff960c5686635600b55
Brian Phillips <brian.phillips201@gmail.com>
2020-04-12 16:47:31 -0700

    feat(docs): added basic Markdown support for pages

--- packages/docs/src/templates/page.js
+++ packages/docs/src/templates/page.js
@@ -2,32 +2,18 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { INLINES } from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
 import SEO from '../components/seo';
+import MarkdownWithOverrides from '../components/MarkdownWithOverrides/MarkdownWithOverrides';
 
 const PageTemplate = props => {
     const { title, content } = props.pageContext.page;
-    const options = {
-        renderNode: {
-            [INLINES.HYPERLINK]: node => {
-                const text = node.content[0].value;
-                const linkTo = node.data.uri;
-                if (linkTo.includes('http')) {
-                    return (
-                        <a href={linkTo} target="_blank" rel="noopener noreferrer nofollow">
-                            {text}
-                        </a>
-                    );
-                }
 
-                return <Link to={linkTo}>{text}</Link>;
-            },
-        },
-    };
     return (
         <>
             <SEO title={title} />
             <h1>{title}</h1>
-            {documentToReactComponents(content.json, options)}
+            <MarkdownWithOverrides>{content.content}</MarkdownWithOverrides>
         </>
     );
 };
