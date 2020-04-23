69597c381e0bd822e5ec9e21a716a52e4e8b4b50
Brian Phillips <brian.phillips201@gmail.com>
2020-04-23 11:58:04 -0700

    fix(docs): changed prod Contentful env for pre-launch

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
                 spaceId: '2rb53inahems',
                 accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
                 downloadLocal: true,
-                environment: process.env.NODE_ENV === 'development' ? 'page-updates' : 'master',
+                environment: process.env.NODE_ENV === 'development' ? 'page-updates' : 'page-updates', // change back to master after full deploy
             },
         },
         `@contentful/gatsby-transformer-contentful-richtext`,
