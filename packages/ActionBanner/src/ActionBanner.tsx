972b2594b569aaac36b4aec40a6a6c59d88ed821
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2020-09-30 09:47:09 -0700

    refactor(action-banner): using internal show to allow animation to complete before hiding elements

--- packages/ActionBanner/src/ActionBanner.tsx
+++ packages/ActionBanner/src/ActionBanner.tsx
@@ -78,7 +78,7 @@ export const ActionBanner: FC<ActionBannerProps> = forwardRef(
 
         return (
             <div
-                style={{ height: show ? height : 0, visibility: show ? 'visible' : 'hidden' }}
+                style={{ height: show ? height : 0, visibility: internalShow ? 'visible' : 'hidden' }}
                 ref={containerRef}
                 aria-hidden={!internalShow}
                 className={styles.container}
