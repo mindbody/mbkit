c779e860c532e5512b66dc32723712846a72e74f
John Costanzo <jrock2004@gmail.com>
2021-11-01 14:43:30 -0400

    feat: adding optional props for location and color

---
+++ packages/Toaster/src/Toaster.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Toaster, ToasterProps } from './Toaster';
+
+export default {
+    title: 'Components/Toaster',
+    component: Toaster,
+} as Meta;
+
+const Template: Story<ToasterProps> = args => <Toaster {...args}>Sample Message</Toaster>;
+
+const defaultArgs: ToasterProps = {
+    show: true,
+};
+
+export const standard = Template.bind({});
+standard.args = {
+    ...defaultArgs,
+};
+
+export const topCenter = Template.bind({});
+topCenter.args = {
+    ...defaultArgs,
+    location: 'top-center',
+};
+
+export const successColor = Template.bind({});
+successColor.args = {
+    ...defaultArgs,
+    color: 'success',
+};
